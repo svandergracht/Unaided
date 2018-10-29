1e63d89a690542ad4c1141c667cd9d3ca1484609
eborchers <borchers.erik@gmail.com>
2018-10-29 17:47:58 -0400

    Forgot to include these files on the previous commit.

---
+++ Assets/Scripts/BattleShipTile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleShipTile : MonoBehaviour {
+
+    [SerializeField] GameObject tileObj;
+    //Mesh renderer that controls the color of the cube
+    public MeshRenderer meshRenderer;
+
+    private CubeState currentState;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    private void Awake()
+    {
+        currentState = new NeutralState(this);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    class NeutralState : CubeState {
+       
+        public NeutralState(BattleShipTile tile) : base(tile) {
+            tile.meshRenderer.material.color = Color.gray;
+
+        }
+    }
+
+    class IncomingState : CubeState
+    {
+
+        public IncomingState(BattleShipTile tile) : base(tile)
+        {
+            tile.meshRenderer.material.color = Color.red;
+
+        }
+    }
+
+    class SuccessState : CubeState
+    {
+
+        public SuccessState(BattleShipTile tile) : base(tile)
+        {
+            tile.meshRenderer.material.color = Color.green;
+
+        }
+    }
+}
---
+++ Assets/Scripts/CubeState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CubeState {
+    // A reference to the containing class.
+    protected BattleShipTile tile;
+
+    public CubeState(BattleShipTile tileObj)
+    {
+        tile = tileObj;
+    }
+
+    // Each state may have an update
+    public virtual void Update() {
+
+    }
+
+    // States may or may not override this.
+    public virtual void OnTriggerEnter(Collider other)
+    {
+    }
+}
